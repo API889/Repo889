b31a0120721bac0c98cdb29577486a442aa25d0c
API889 <75469776+API889@users.noreply.github.com>
2020-12-29 10:22:37 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,11 +1,7 @@
 public class Demo{
 public static void main(String args[])
 {
-System.out.println("Hello I am from Git No1");
-System.out.println("Hello I am from Git No2");
-System.out.println("Hello I am from Git No3");
-System.out.println("Hello I am from Git No4");
-System.out.println("Hello I am from Git No5");
-  
+System.out.println("Hello I am from Git Num1");
+System.out.println("Hello I am from Git Num2"); 
 }
 }
