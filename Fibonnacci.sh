6ca34d38ec85fc0fe88152b56aeb291fc431be09
VeenaVangala <veena.vangala@apisero.com>
2021-01-15 16:18:08 +0530

    modified login

--- Fibonnacci.sh
+++ Fibonnacci.sh
@@ -1,21 +1,21 @@
-echo "How many number of terms to be generated :$1"
-function fib
-{
-  x=0
-  y=1
-  i=2
-  n=$1
-  echo "Fibonacci Series up to $n terms :"
-  echo "$x"
-  echo "$y"
-  while [ $i -lt $n ]
-  do
-      i=`expr $i + 1 `
-      z=`expr $x + $y `
-      echo "$z"
-      x=$y
-      y=$z
-  done
-}
-r=`fib $n`
-echo "$r"
+echo "Enter a number : $1"
+
+# First Number of the
+# Fibonacci Series
+a=0
+n=$1
+# Second Number of the
+# Fibonacci Series
+b=1
+ 
+echo "The Fibonacci series is : "
+ 
+for (( i=0; i<n; i++ ))
+do
+ 
+    echo -n "$a "
+    fn=$((a + b))
+    a=$b
+    b=$fn
+ 
+done
